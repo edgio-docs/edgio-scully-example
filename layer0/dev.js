c8b504304b3ec894640a4311e6601df14492cbbf
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2022-08-13 17:32:27 +0530

    Update dev.js

--- layer0/dev.js
+++ layer0/dev.js
@@ -2,7 +2,7 @@ const { createDevServer } = require('@layer0/core/dev')
 
 module.exports = function () {
   return createDevServer({
-    label: 'Scully',
+    label: '[Scully]',
     command: (port) => `npx ng serve -- --port ${port}`,
     ready: [/listening on/i],
   })
